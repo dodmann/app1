c0fe1702134036ae6306bd2d1263145e5040db58
Davi Luciano <davilsa4@gmail.com>
2018-07-05 09:17:26 -0300

    ok

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core'
 @Component({
     selector: 'app-topo',
     templateUrl: './topo.component.html',
-    styles: ['.exemplo{ color:red }']
+    styleUrls: ['./topo.component.css'] 
 })
 export class TopoComponent {
 
