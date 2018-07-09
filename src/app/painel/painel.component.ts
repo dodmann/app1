820172a506d3af5459697785041f6cf1c018b363
Davi Luciano <davilsa4@gmail.com>
2018-07-09 11:25:07 -0300

    ok

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -12,7 +12,7 @@ export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES
   public instrucao: string = 'Traduza a frase: '
-  public resposta : string
+  public resposta : string = ''
 
   public rodada: number = 0
   public rodadaFrase: Frase
@@ -20,8 +20,7 @@ export class PainelComponent implements OnInit {
   public progresso: number = 0
 
   constructor() {
-    this.rodadaFrase = this.frases[this.rodada]
-    console.log(this.rodadaFrase)
+    this.atualizaRodada()
   }
 
   ngOnInit() {
@@ -44,10 +43,19 @@ export class PainelComponent implements OnInit {
     console.log(this.progresso)
 
     //atualiza o objeto rodadaFrase
-    this.rodadaFrase = this.frases[this.rodada]
+    this.atualizaRodada()
+
     } else {
       alert("A tradução está errada")
     }
+  }
+
+  public atualizaRodada() : void{
+    //define a frase da rodada com base em alguma lógica
+    this.rodadaFrase = this.frases[this.rodada]
+    
+    //limpa a rodada
+    this.resposta = ''
   }
 
 }
