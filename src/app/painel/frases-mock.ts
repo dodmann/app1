3881210b613082194dca92c381cd11b3909431c8
Davi Luciano <davilsa4@gmail.com>
2018-07-09 10:55:00 -0300

    ok

--- src/app/painel/frases-mock.ts
+++ src/app/painel/frases-mock.ts
@@ -1,8 +1,8 @@
 import { Frase } from '../shared/frase.model'
 
 export const FRASES: Frase[] = [
-    { fraseEng: 'I like to learn', frasePtBr: 'Eu gosto de aprender' }
-    { fraseEng: 'Who are you?', frasePtBr: 'Quem é você?' }
-    { fraseEng: 'Hi there', frasePtBr: 'Oi' }
+    { fraseEng: 'I like to learn', frasePtBr: 'Eu gosto de aprender' },
+    { fraseEng: 'Who are you?', frasePtBr: 'Quem é você?' },
+    { fraseEng: 'Hi there', frasePtBr: 'Oi' },
     { fraseEng: 'I am a fridge', frasePtBr: 'Eu sou uma geladeira' }
 ]
