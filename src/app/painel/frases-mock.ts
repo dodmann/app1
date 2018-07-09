d8c1a34c364dc2cce8a6d8d2db844af0a6363b42
Davi Luciano <davilsa4@gmail.com>
2018-07-09 09:08:38 -0300

    ok

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,8 @@
+import { Frase } from '../shared/frase.model'
+
+const FRASES: Frase[] = [
+    { fraseEng: 'I like to learn', frasePtBr: 'Eu gosto de aprender' }
+    { fraseEng: 'Who are you?', frasePtBr: 'Quem é você?' }
+    { fraseEng: 'Hi there', frasePtBr: 'Oi' }
+    { fraseEng: 'I am a fridge', frasePtBr: 'Eu sou uma geladeira' }
+]
